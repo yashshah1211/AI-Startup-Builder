7e5c2f300b9cff55b2b2ab46608b3eeb51b4fb31
Yahh Shah <yashshah7117@gmail.com>
2026-06-20 23:59:04 +0530

    Add Production CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://YOUR-VERCEL-APP.vercel.app"],
+                   "https://ai-startup-builder-ivory.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
